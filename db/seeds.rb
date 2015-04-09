b9aa677e7eadddc9f645ddd9e8ddf38bb0759106
Jose Rosado <joserosado@esd.com.do>
2015-04-09 13:15:33 -0400

    cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,5 @@
 require 'faker'
 
-# # Create Posts
-# 50.times do
-#   Post.create!(
-#     title:  Faker::Lorem.sentence,
-#     body:   Faker::Lorem.paragraph
-#   )
-# end
-# posts = Post.all
-
-# # Create Comments
-# 100.times do
-#   Comment.create!(
-#     post: posts.sample,
-#     body: Faker::Lorem.paragraph
-#   )
-# end
-
 post = Post.find_by(title: 'Unique post2')
 
 if !post
