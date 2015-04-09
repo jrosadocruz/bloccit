3b25b3b1489c15910f05a5ddb1a84fa4b95de938
Jose Rosado <joserosado@esd.com.do>
2015-04-09 13:14:12 -0400

    Finished seed-data-assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,36 @@
 require 'faker'
 
-# Create Posts
-50.times do
+# # Create Posts
+# 50.times do
+#   Post.create!(
+#     title:  Faker::Lorem.sentence,
+#     body:   Faker::Lorem.paragraph
+#   )
+# end
+# posts = Post.all
+
+# # Create Comments
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+
+post = Post.find_by(title: 'Unique post2')
+
+if !post
   Post.create!(
-    title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
+    title: "Unique post2",
+    body: Faker::Lorem.paragraph
   )
 end
-posts = Post.all
 
-# Create Comments
-100.times do
+post = Post.find_by(title: 'Unique post2')
+
+if post.comments.length < 1
   Comment.create!(
-    post: posts.sample,
+    post: post,
     body: Faker::Lorem.paragraph
   )
 end
