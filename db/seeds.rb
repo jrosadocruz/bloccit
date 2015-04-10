679d0e38fe65d0c566cb37f6d89ee3671c65e50d
Jose Rosado <joserosado@esd.com.do>
2015-04-10 10:24:55 -0400

    Crud assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,31 @@
 require 'faker'
 
 # Create Posts
-50.times do
-  Post.create!(
-    title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
-  )
-end
-posts = Post.all
+# 50.times do
+#   Post.create!(
+#     title:  Faker::Lorem.sentence,
+#     body:   Faker::Lorem.paragraph
+#   )
+# end
+# posts = Post.all
 
 # Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+
+# Create ads
+50.times do
+  Advertisement.create!(
+    title:  Faker::Lorem.sentence,
+    copy:   Faker::Lorem.paragraph,
+    price:  Faker::Number.number(3)
   )
 end
+ads = Advertisement.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
