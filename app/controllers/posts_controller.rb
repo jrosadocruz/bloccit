9b15d12e73789794872351116cd1c9958e8d72fd
Jose Rosado <joserosado@esd.com.do>
2015-04-23 15:44:50 -0400

    Finishes checkpoint 32

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+
   def update
     @post = Post.find(params[:id])
     if @post.update_attributes(params.require(:post).permit(:title, :body))
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,27 @@
 require 'faker'
 
-# Create Posts
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 50.times do
   Post.create!(
-    title:  Faker::Lorem.sentence,
-    body:   Faker::Lorem.paragraph
+    user:  users.sample,
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph
   )
 end
+
 posts = Post.all
 
-# Create Comments
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -17,6 +29,14 @@ posts = Post.all
   )
 end
 
-puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+  email: 'jose@rosa.do',
+  password: 'j4445479'
+)
+
+puts "Seed finished!"
+puts "#{User.count} users created."
+puts "#{Post.count} posts created."
+puts "#{Comment.count} comments created."
