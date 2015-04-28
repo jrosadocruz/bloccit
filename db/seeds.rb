37b11c53891e5ee4fbbef461af026a8ecb4e90ca
Jose Rosado <joserosado@esd.com.do>
2015-04-28 11:05:01 -0400

    Finished checkpoint 34

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,9 +12,18 @@ end
 
 users = User.all
 
+15.times do
+  Topic.create!(
+    name:        Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
     user:  users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
@@ -29,12 +38,37 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.skip_confirmation!
-user.update_attributes!(
-  email: 'jose@rosa.do',
-  password: 'j4445479'
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'holamundo!',
+  role:     'admin'
 )
+
+admin.skip_confirmation!
+admin.save!
+
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com',
+  password: 'holamundo!',
+  role:     'moderator'
+)
+
+moderator.skip_confirmation!
+moderator.save!
+
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'holamundo!',
+  role:     'member'
+)
+
+member.skip_confirmation!
+member.save!
+
+
 
 puts "Seed finished!"
 puts "#{User.count} users created."
