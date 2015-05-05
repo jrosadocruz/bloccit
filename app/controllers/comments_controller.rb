578585c285e46d512b92beb6456daa73ec96a770
Jose Rosado <joserosado@esd.com.do>
2015-05-05 14:04:41 -0400

    Complete comments assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,22 @@
 class CommentsController < ApplicationController
   def new
     @topic = Topic.find(params[:topic_id])
-    @post  = Post.find(params[:user_id])
+    @post  = @topic.posts.find(params[:user_id])
     @comment = @post.comments.new
   end
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post  = Post.find(params[:post_id])
+    @post  = @topic.posts.find(params[:post_id])
     @comment = @post.comments.build(comments_params)
     @comment.user = current_user
+    authorize @comment
     if @comment.save
       flash[:notice] = "Comment saved"
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving comment. Please try again."
-      render :new
+      redirect_to [@topic, @post]
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
+
+  validates :body, length: { minimum: 20 }, presence: true
 end
