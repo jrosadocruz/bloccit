2ccafeafc01ce34005809d1caf6d1371754aabd8
Jose Rosado <joserosado@esd.com.do>
2015-05-05 10:29:40 -0400

    Still not working

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem 'figaro', '1.0'
 gem 'pundit'
 gem 'redcarpet'
 
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428135233) do
+ActiveRecord::Schema.define(version: 20150505132032) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20150428135233) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "summaries", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "post_id"
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
@@ -77,6 +84,7 @@ ActiveRecord::Schema.define(version: 20150428135233) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
