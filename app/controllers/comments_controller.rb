061bf1933ad8450a6e262251bbdedbad548e8474
Jose Rosado <joserosado@esd.com.do>
2015-05-06 11:17:44 -0400

    Finished assignment 41

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,37 +1,34 @@
 class CommentsController < ApplicationController
   def new
-    @topic = Topic.find(params[:topic_id])
     @post  = @topic.posts.find(params[:user_id])
     @comment = @post.comments.new
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    @post  = @topic.posts.find(params[:post_id])
+    @post  = Post.find(params[:post_id])
     @comment = @post.comments.build(comments_params)
     @comment.user = current_user
     authorize @comment
     if @comment.save
       flash[:notice] = "Comment saved"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving comment. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     authorize @comment
 
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "Comment could't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
