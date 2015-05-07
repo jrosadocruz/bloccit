482e45618b70015227d91fa89a23f7061111cd4c
Jose Rosado <joserosado@esd.com.do>
2015-05-07 18:53:22 -0400

    Levi's awsome

--- spec/model/vote_spec.rb
+++ spec/model/vote_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 describe Vote do
   describe "validations" do
+
     describe "value validation" do
-      it "only allow -1 or 1 as values" do
-        expect( @vote ).to eq([-1,1])
+      it "allows 1 as it's value" do
+        @vote = Vote.new(value: 1)
+        expect(@vote.valid?).to eq(true)
+      end
+      it "allows -1 as it's value" do
+        @vote = Vote.new(value: -1)
+        expect(@vote.valid?).to eq(true)
+      end
+      it "does not allow 2 as it's value" do
+        @vote = Vote.new(value: 2)
+        expect(@vote.valid?).to eq(false)
+      end
+      it "does not allow 0 as it's value" do
+        @vote = Vote.new(value: 0)
+        expect(@vote.valid?).to eq(false)
       end
     end
   end
