cb834897acf92eba950f4807ffc7e055e2a4b787
Jose Rosado <joserosado@esd.com.do>
2015-05-27 09:29:34 -0400

    votes spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505160552) do
+ActiveRecord::Schema.define(version: 20150527130358) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -84,5 +84,13 @@ ActiveRecord::Schema.define(version: 20150505160552) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
