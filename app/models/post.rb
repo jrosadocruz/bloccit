8cc6865f03de467cbda2cbc78754d152f350fa47
Jose Rosado <joserosado@esd.com.do>
2015-05-29 16:21:16 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
+  default_scope { order('rank DESC') }
+
   def up_votes
     self.votes.where(value: 1).count
   end
@@ -16,7 +18,12 @@ class Post < ActiveRecord::Base
     self.votes.sum(:value)
   end
 
-  default_scope { order('created_at DESC') }
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
+
 
   scope :for_user, -> (user)  { where(user_id: user.id)  }
   scope :by_email, -> (email) { joins(:user).where(:users => {email: email})}
