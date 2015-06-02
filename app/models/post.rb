61819a5dbef9f75e1083ba48151cd835e5dc38c1
Jose Rosado <joserosado@esd.com.do>
2015-06-02 11:15:52 -0400

    not much

--- app/models/post.rb
+++ app/models/post.rb
@@ -33,6 +33,13 @@ class Post < ActiveRecord::Base
   validates :title , length: { minimum: 5 }  , presence: true
   validates :body  , length: { minimum: 20 } , presence: true
 
+  # validates :topic , presence: true
+  # validates :user  , presence: true
+
+  # def self.for_user user
+  #   where(user_id: user.id)
+  # end
+
   private
 
   def create_vote
