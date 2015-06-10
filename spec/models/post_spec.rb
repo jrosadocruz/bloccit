03f6a577f7ccfb3caf75624c8cf8624016cdfea9
Jose Rosado <joserosado@esd.com.do>
2015-06-10 11:47:38 -0400

    Finished checkpoint

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe Post do
+  include TestFactories
   describe "vote methods" do
 
     before do
@@ -36,23 +37,4 @@ describe Post do
       end
     end
   end
-end
-
-
-def associated_post(options={})
-  post_options = {
-    title: 'Post title',
-    body: 'Post bodies must be pretty long.',
-    topic: Topic.create(name: 'Topic name'),
-    user: authenticated_user
-  }.merge(options)
-  Post.create(post_options)
-end
-
-def authenticated_user(options={})
-  user_options = {email: "email#{rand}@fake.com", password: 'password'}.merge(options)
-  user = User.new(user_options)
-  user.skip_confirmation!
-  user.save
-  user
 end
