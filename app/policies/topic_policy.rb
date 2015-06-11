954bc28257dcef83c00b495540bfb015695cb748
Jose Rosado <joserosado@esd.com.do>
2015-06-11 14:31:51 -0400

    checkpoint done

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -2,9 +2,15 @@ class TopicPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    record.public? || user.present?
+  end
+
   def create?
     user.present? && user.admin?
   end
+
   def update?
     create?
   end
