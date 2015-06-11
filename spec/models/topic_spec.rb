4e77f751b554def9237650181a48a0cb7e6aaba2
Jose Rosado <joserosado@esd.com.do>
2015-06-11 15:01:34 -0400

    Spec done.

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe Topic do
+  describe "scopes" do
+
+    before do
+      @public_topic  = Topic.create # default is public
+      @private_topic = Topic.create(public: false)
+      @topics_count  = Topic.all.count
+    end
+
+    describe "publicly_viewable" do
+      it "returns a relation of all public topics" do
+        expect(Topic.publicly_viewable).to eq( [@public_topic] )
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns a relation of all private topics" do
+        expect(Topic.privately_viewable).not_to eq( [@public_topic] )
+      end
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        u = true # sneaky solution; we don't need a real user, just something truthy
+        topic_size = Topic.visible_to(u).count
+        expect(topic_size).to eq(@topics_count)
+      end
+
+      it "returns only public topics if user is nil" do
+        user = nil
+        expect(Topic.visible_to(user).count).to be < @topics_count
+      end
+    end
+  end
+end
