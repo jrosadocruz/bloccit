27671703e62c26bc54010956b228809aa2102ef1
Jose Rosado <joserosado@esd.com.do>
2015-06-12 11:46:21 -0400

    Assignment complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,17 @@ module ApplicationHelper
     redcarpet  = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def up_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).up_vote?
+      'voted'
+    end
+  end
+
+  def down_vote_link_classes(post)
+    if current_user.voted(post) && current_user.voted(post).down_vote?
+      'voted'
+    end
+  end
+
 end
